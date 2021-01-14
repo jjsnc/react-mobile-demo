37517273333650434c8136dfe50c131c0fd5c1e2
Damon <zhuowangyi@163.com>
2021-01-14 18:01:17 +0800

    init

--- src/pages/active618/view/home/index.js
+++ src/pages/active618/view/home/index.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 import "./index.less";
 class Home extends React.Component {
-    componentDidMount() {
-    }
-    render() {
-        return (
-            <div className="test">Home</div>
-        )
-    }
-
+  componentDidMount() {}
+  render() {
+    return (
+      <div className="wrap">
+        <div className="fixed-section">
+          <input className="input"></input>
+        </div>
+        <div className="header-section">
+          <div className="title">酒景打包订</div>
+          <div className="sub-title">“省心更优惠”</div>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapState = (state) => ({
-})
-const mapDispatch = (dispatch) => ({
-
-});
+const mapState = (state) => ({});
+const mapDispatch = (dispatch) => ({});
 
 export default connect(mapState, mapDispatch)(Home);
