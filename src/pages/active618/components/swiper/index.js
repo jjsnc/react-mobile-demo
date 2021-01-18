4c8fff0189f24bd307c92f180a5a8df7ef0c6bbd
Damon <zhuowangyi@163.com>
2021-01-18 19:51:57 +0800

    init

--- src/pages/active618/components/swiper/index.js
+++ src/pages/active618/components/swiper/index.js
@@ -1,20 +1,42 @@
 import React, { Component } from "react";
 import Swiper from "react-id-swiper";
-import 'swiper/swiper.less'
+import "swiper/swiper.less";
+import "./index.less";
 export default class MySwiper extends Component {
-  handleSwiper(value) {
-    console.log(value);
+  constructor(props) {
+    super(props);
+    this.state = {
+      realIndex: 0,
+      list: [1, 2, 3, 4, 5, 6],
+    };
+    this.handleSlideChange = this.handleSlideChange.bind(this);
+  }
+  handleSlideChange(realIndex) {
+    this.setState({
+      realIndex,
+    });
   }
   render() {
+    const params = {
+      on: {
+        slideChange: (event) => {
+          this.handleSlideChange(event.realIndex);
+        },
+      },
+    };
+    let { list,realIndex } = this.state;
     return (
-      <div className="box">
-        <Swiper>
-          <div>Slide #1</div>
-          <div>Slide #2</div>
-          <div>Slide #3</div>
-          <div>Slide #4</div>
-          <div>Slide #5</div>
+      <div className="swiper-article">
+        <Swiper {...params} onChange={this.handleSlider}>
+          {list.map((item) => {
+          return  <div key={item}>Slide #1</div>;
+          })}
         </Swiper>
+        <div className="dots-list">
+          <div>{realIndex+1}</div>
+          <div>/</div>
+          <div>{list.length}</div>
+        </div>
       </div>
     );
   }
